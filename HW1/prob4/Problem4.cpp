f4f4b1a11aae59adee9d26054cf3a732cab485e9
pshriwise <shriwise@wisc.edu>
2014-09-18 13:50:25 -0500

    Implemented the De Casteljau algorithm! Time to work on getting the data in a way that is nicely plottable.

--- HW1/prob4/Problem4.cpp
+++ HW1/prob4/Problem4.cpp
@@ -4,164 +4,88 @@
 #include <iostream>
 #include <fstream>
 #include <armadillo>
+#include <assert.h>
 
 using namespace arma;
 
 
-void create_gnuplot_script( std::vector<std::string> filenames );
+void de_cast( double u, Mat<double> CP, Mat<double> &pnt );
 
 int main(int argc, char** argv)
 {
 
-  int u_pnts= 50;
+  double u = 0.5;
+
+  // setup the point matrix
+  Mat<double> P;
+  P << 0 << 0 << 0 << endr
+    << 1 << 2 << 0 << endr
+    << 3 << 5 << 0 << endr
+    << 4 << 4 << 0 << endr
+    << 5 << 0 << 0 << endr;
+
+  P = P.t();
+  std::cout << P.col(0) << std::endl;
+
+  std::vector<Mat<double> > plot_data;
+
+  Mat<double> pnt(3,1);
+  de_cast( u, P, pnt );
+
+  std::cout << pnt << std::endl;
+
+  return 0;
+}
 
 
-  std::vector<std::string> filenames;
 
-      std::cout << "--------------------------------------" << std::endl;
+void de_cast( double u, Mat<double> CP, Mat<double> &pnt)
+{
 
+  assert(3 == CP.n_rows);
 
-      //create the point array (5 points)
-      
-      
-      // setup the point matrix
-      double p[15]= { 0,        0,        0,
-		      1,        2,        0,
-		      3,        5,        0,
-		      4,        4,        0,
-                      5,        0,        0 };
-  
-      Mat<double> P(p,3,5,false);
-      P=P.t(); // have to do this because of the way that armadillo reads values into matrices
-      std::cout << "P Matrix:" << std::endl;
-      std::cout << P << std::endl;
-      
-      // now setup the coefficients for the basis matrix
-      double b[25]   =  {    1,  -4,   6,  -4, 1,
-			    -4,  12, -12,   4, 0, 
-			     6, -12,   6,   0, 0,
-			    -4,   4,   0,   0, 0, 
-			     1,   0,   0,   0, 0 };
-      
-      Mat<double> B(b,5,5,false);
-      B=B.t(); // have to do this because of the way that armadillo reads values into matrices
-      std::cout << "B Matrix:" << std::endl;
-      std::cout << B << std::endl;
-      
+  if( CP.n_cols == 1) 
+    {
 
-      //// PART C \\\
+      pnt = CP;
+      return;
 
-      //setup the parameter sweep along u
-      Mat<double> T;
-      T.set_size(5,u_pnts);
+    }
+  else
+    {
+      //create a new matrix for storing the values
+      int new_cols = CP.n_cols - 1;
+      Mat<double> N(3, new_cols);
       
-      double m = 1/(double(u_pnts)-1);
-      
-      for(int i=0; i < u_pnts; i++)
+      for( unsigned int i = 0; i < new_cols; i++) 
 	{
-	  double t = double(i)*m;
-	  T(0,i) = pow(t,4);
-	  T(1,i) = pow(t,3);
-	  T(2,i) = pow(t,2);
-	  T(3,i) = t;
-	  T(4,i) = 1;
-	}
-      
-      T=T.t();  // doing this so that the martix has the right form (n x 5)
-      Mat<double> Q = T*B*P;
-      
-      //transform the answer so its easier to write to a file...
-      Q=Q.t();
-      
-      std::ofstream data_file;
+	  //extract the vectors we want
+	  vec A(3);
+	  A(0) = CP(0,i);
+	  A(1) = CP(1,i);	    
+	  A(2) = CP(2,i);	    
 
-      //create the filename
-      std::ostringstream data_filename;
-      data_filename << "Problem3.dat";
-      filenames.push_back( data_filename.str() );
-     
-      data_file.open(&(data_filename.str()[0]) );
-
-      for(unsigned int i = 0; i < Q.n_cols; i++)
-	{
-
-	  data_file << Q(0,i) << "\t" << Q(1,i) << "\t" << Q(2,i) << "\n";
+	  vec B(3);
+	  B(0) = CP(0, i+1);
+	  B(1) = CP(1, i+1);
+	  B(2) = CP(2, i+1);
+	  
+	  //calculate the new vectors
+	  vec new_vec = A + u*(B-A);
+	  
+	  //insert this new vector into the new matrix
+	  N(0,i) = new_vec(0);
+	  N(1,i) = new_vec(1);
+	  N(2,i) = new_vec(2);
 	  
 	}
-
-      data_file.close();
-
-      std::cout << "--------------------------------------" << std::endl;
       
-      //// PART B \\\\
+      de_cast( u, N, pnt );
 
-      //write out the x,y,z values for u=0.2,0.5,0.8
-      double u[3] = {0.2, 0.5, 0.8};
-
-      Mat<double> U;
-      U.set_size(5,3);
-      for( unsigned int i = 0; i < 3; i++)
-	{
-	  U(0,i) = pow(u[i],4);
-	  U(1,i) = pow(u[i],3);
-	  U(2,i) = pow(u[i],2);
-	  U(3,i) = u[i];
-	  U(4,i) = 1;
-
-	}
-
-      //reset the data points
-      Q= U.t()*B*P;
-
-      Q=Q.t();
-      
-      std::cout << std::endl;
-
-      for( unsigned int i = 0; i < Q.n_cols; i++)
-	std::cout << "When u=" << u[i] << " the curve point is (" << Q(0,i) << ", " << Q(1,i) << ", " << Q(2,i) << ")" << std::endl;
+    }
 
 
-  create_gnuplot_script( filenames );
-  
-  return 0;
 
-  
+
+
 }
-
-
-void create_gnuplot_script( std::vector<std::string> filenames ) 
-{
-  
-  std::ofstream gp_script;
-  
-  gp_script.open("Problem3_gp.p");
-
-  gp_script << "#Name: Patrick Shriwise \t Date: 9/22/14 \n" ;
-  gp_script << "#This is a gnuplot file for ploting data for Problem 3 in HW 1 \n";
-  gp_script << "#for ME 535 at the University of Wisconsin - Madison\n \n";
-
-  gp_script << "#This script plots a 4th degree Bezier curve using the points:\n";
-  gp_script << "# [0,0;1,2;3,5;4,4;5,0]\n";
-
-  gp_script << "plot ";
-  for(unsigned int i = 0; i < filenames.size(); i++){
-    gp_script << "'" << filenames[i] << "' using 1:2";
-      if( i != filenames.size()-1) gp_script <<", \\\n";
-  }
-  gp_script << "\n";
-  
-  gp_script << "set xlabel 'x' \n";
-  gp_script << "set ylabel 'y' \n";
-  gp_script << "set zlabel 'z' \n";
-  gp_script << "set title 'HW1_Problem3 Bezier Curve' \n";
-  gp_script << "set mytics '4' \n";
-  gp_script << "set mxtics '4' \n";
-  gp_script << "set nokey \n";
-
-  //no more settings after here
-  gp_script << "replot \n";
-  gp_script << "set output 'Problem3.png'\n";
-  gp_script << "\n"; gp_script << "pause -1";
-  gp_script.close();
-}
-
