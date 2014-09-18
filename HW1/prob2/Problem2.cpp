2f0ce92a600b863660096369f72cca3ea9ee9ccb
pshriwise <shriwise@wisc.edu>
2014-09-18 10:19:53 -0500

    Updates to how we create the data points and the gnuplot script input.

--- HW1/prob2/Problem2.cpp
+++ HW1/prob2/Problem2.cpp
@@ -22,14 +22,24 @@ int main(int argc, char** argv)
       //[x0, x1, dx0, dx1; y0 y1l dy0, dy1; z0, z1, dz0, dz1]
       
       double m = 1/(double(u_pnts)-1);
-      
+
+
+
+      //create the data points here
+      std::vector<double> x,y,z;
+
       for(int i=0; i < u_pnts; i++)
 	{
 	  double t = double(i)*m;
+	  //polynomial for x
+	  x.push_back( t + 9*pow(t,2) - 6*pow(t,3) );
+	  y.push_back( t + 4*pow(t,2) - 5*pow(t,3) );
+	  z.push_back( 0 );
 
 	}
       
       std::ofstream data_file;
+
 
       //create the filename
       std::ostringstream data_filename;
@@ -38,18 +48,16 @@ int main(int argc, char** argv)
      
       data_file.open(&(data_filename.str()[0]) );
 
-      for(unsigned int i = 0; i < Q.n_cols; i++)
+
+      for(unsigned int i = 0; i < x.size(); i++)
 	{
 
-	  data_file << Q(0,i) << "\t" << Q(1,i) << "\t" << Q(2,i) << "\n";
+	  data_file << x[i] << "\t" << y[i] << "\t" << z[i] << "\n";
 	  
 	}
 
       data_file.close();
 
-      std::cout << "--------------------------------------" << std::endl;
-      
-      
 
 
   create_gnuplot_script( filenames );
@@ -68,15 +76,16 @@ void create_gnuplot_script( std::vector<std::string> filenames )
   gp_script.open("Problem1_gp.p");
 
   gp_script << "#Name: Patrick Shriwise \t Date: 9/22/14 \n" ;
-  gp_script << "#This is a gnuplot file for ploting data for Problem 1 in HW 1 \n";
+  gp_script << "#This is a gnuplot file for ploting data for Problem 2 in HW 1 \n";
   gp_script << "#for ME 535 at the University of Wisconsin - Madison\n \n";
 
-  gp_script << "#This file will generate the plot for each k in a new color with \n";
-  gp_script << "#with a legend to indicate which curve matches which k. \n";
+  gp_script << "# This will plot the Hermite curve with P0 = (0,0,0) P1 = (4,0,0)\n";
+  gp_script << "# P'0 = (1,4,0) and P'1 = (1,-4,0). It does so using the polynomial\n";
+  gp_script << "# rather than the matrix forms as before\n";
 
   gp_script << "plot ";
   for(unsigned int i = 0; i < filenames.size(); i++){
-    gp_script << "'" << filenames[i] << "' using 1:2" << " title " << "'" << filenames[i].substr(9,100) << "'";
+    gp_script << "'" << filenames[i] << "' using 1:2";
       if( i != filenames.size()-1) gp_script <<", \\\n";
   }
   gp_script << "\n";
@@ -84,7 +93,7 @@ void create_gnuplot_script( std::vector<std::string> filenames )
   gp_script << "set xlabel 'x' \n";
   gp_script << "set ylabel 'y' \n";
   gp_script << "set zlabel 'z' \n";
-  gp_script << "set title 'HW1_Problem1' \n";
+  gp_script << "set title 'HW1_Problem2' \n";
   gp_script << "set mytics '4' \n";
   gp_script << "set mxtics '4' \n";
   
