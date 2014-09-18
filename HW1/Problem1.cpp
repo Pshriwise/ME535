d15804a5a57242a688b405d9818bb0a8129c6d9e
pshriwise <shriwise@wisc.edu>
2014-09-17 22:58:14 -0500

    Added creation of a gnuplot script for the Problem1.

--- HW1/Problem1.cpp
+++ HW1/Problem1.cpp
@@ -7,6 +7,9 @@
 
 using namespace arma;
 
+
+void create_gnuplot_script( std::vector<std::string> filenames );
+
 int main(int argc, char** argv)
 {
 
@@ -15,6 +18,7 @@ int main(int argc, char** argv)
   std::vector<double> ks(4);
   ks[0] = 0.5; ks[1] = 1; ks[2] =  1.5; ks[3]= 2;
 
+  std::vector<std::string> filenames;
   for( std::vector<double>::iterator i = ks.begin(); i!=ks.end(); i++)
     {
       double k = *i;
@@ -72,7 +76,8 @@ int main(int argc, char** argv)
       //create the filename
       std::ostringstream data_filename;
       data_filename << "Problem1_k=" << k << ".dat";
-
+      filenames.push_back( data_filename.str() );
+     
       data_file.open(&(data_filename.str()[0]) );
 
       for(unsigned int i = 0; i < Q.n_cols; i++)
@@ -89,7 +94,7 @@ int main(int argc, char** argv)
       
     }
 
-
+  create_gnuplot_script( filenames );
   
   return 0;
 
@@ -97,13 +102,37 @@ int main(int argc, char** argv)
 }
 
 
-void create_gnuplot_script( std::string filename ) 
+void create_gnuplot_script( std::vector<std::string> filenames ) 
 {
-
+  
   std::ofstream gp_script;
   
-  gp_script.open("Problem1_plot");
+  gp_script.open("Problem1_gp.p");
 
+  gp_script << "#Name: Patrick Shriwise \t Date: 9/22/14 \n" ;
+  gp_script << "#This is a gnuplot file for ploting data for Problem 1 in HW 1 \n";
+  gp_script << "#for ME 535 at the University of Wisconsin - Madison\n \n";
+
+  gp_script << "#This file will generate the plot for each k in a new color with \n";
+  gp_script << "#with a legend to indicate which curve matches which k. \n";
+
+  gp_script << "plot ";
+  for(unsigned int i = 0; i < filenames.size(); i++){
+    gp_script << "'" << filenames[i] << "' using 1:2" << " title " << "'" << filenames[i].substr(9,100) << "'";
+      if( i != filenames.size()-1) gp_script <<", \\\n";
+  }
+  gp_script << "\n";
   
-
+  gp_script << "set xlabel 'x' \n";
+  gp_script << "set ylabel 'y' \n";
+  gp_script << "set zlabel 'z' \n";
+  gp_script << "set title 'HW1_Problem1' \n";
+  gp_script << "set mytics '4' \n";
+  gp_script << "set mxtics '4' \n";
+  
+  //no more settings after here
+  gp_script << "replot \n";
+  gp_script << "\n"; gp_script << "pause -1";
+  gp_script.close();
 }
+
