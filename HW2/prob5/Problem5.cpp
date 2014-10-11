2741ac26e15a0369af5daf5d6a60c1e5ec8caf9d
Pshriwise <shriwise@wisc.edu>
2014-10-11 00:40:40 -0500

    Added a function for calculating the derivatve of the BSpline.

--- HW2/prob5/Problem5.cpp
+++ HW2/prob5/Problem5.cpp
@@ -8,9 +8,12 @@
 
 using namespace arma;
 
-void blossom_de_boor( int degree, Mat<double> cps, std::vector<double> knots, double u, Mat<double> &pnt);
+void blossom_de_boor( int degree, Mat<double> cps, std::vector<double> knots, double u, Mat<double> &pnt, bool deriv = false);
 
 void find_pnt( Mat<double> base, std::vector<double> knots, Mat<double> &pnt, double u );
+
+
+void find_slope( Mat<double> base, std::vector<double> knots, Mat<double> deriv, double u);
 
 int main( int argc, char** argv)
 {
@@ -49,7 +52,6 @@ int main( int argc, char** argv)
     {
       blossom_de_boor( degree, CPs, knots, u, pnt);
       datafile << pnt(0) << "\t" << pnt(1) <<  "\t" << pnt(2) << std::endl; 
-
     }
 
   datafile.close();
@@ -58,7 +60,14 @@ int main( int argc, char** argv)
   CPfile << CPs.t();
   CPfile.close();
 
+  part_header("B");
+  
+  double t = 3;
+  std::cout << "The derivative of the B-Spline at t= " << t << ":" << std::endl; 
 
+  blossom_de_boor(degree, CPs, knots, t, pnt, true);
+
+  std::cout << pnt << std::endl; 
 
 
   return 0;
@@ -66,7 +75,7 @@ int main( int argc, char** argv)
 }
 
 
-void blossom_de_boor( int degree, Mat<double> cps, std::vector<double> knots, double u, Mat<double> &pnt )
+void blossom_de_boor( int degree, Mat<double> cps, std::vector<double> knots, double u, Mat<double> &pnt, bool deriv )
 {
 
 
@@ -101,8 +110,15 @@ void blossom_de_boor( int degree, Mat<double> cps, std::vector<double> knots, do
   sub_knots.insert(sub_knots.begin(), knots.begin()+interval, knots.begin()+interval+(degree*2) );
 
 
-  find_pnt( base, sub_knots, pnt, u);
-
+  if(deriv)
+    {
+      find_slope(base, sub_knots, pnt, u);
+      pnt= degree*pnt;
+    }
+  else
+    {
+      find_pnt( base, sub_knots, pnt, u);
+    }
 }
 
 
@@ -129,11 +145,6 @@ void find_pnt( Mat<double> base, std::vector<double> knots, Mat<double> &pnt, do
 
       new_base.col(i)= coeff1*base.col(i) + coeff2*base.col(i+1);
 
-      
-     
-     
-     
-
     }
       
   //remove the fisrt and last knot before the recursive call 
@@ -146,5 +157,37 @@ void find_pnt( Mat<double> base, std::vector<double> knots, Mat<double> &pnt, do
 }
       
       
+void find_slope( Mat<double> base, std::vector<double> knots, Mat<double> deriv, double u)
+{
+
+  if ( 2 == base.n_cols ) 
+    {
+      deriv = (1/(knots[1]-knots[0]))*(base.col(1)-base.col(0));
+      return;
+    }
+  int offset = base.n_cols-1; 
+
+
+  Mat<double> new_base(3,offset);
+
+
+  for( unsigned int i = 0 ; i < offset ; i ++)
+    {
+      double coeff1 = (double(knots[i+offset])-u)/double(knots[i+offset]-knots[i]);
+
+      double coeff2 = (u - double(knots[i]))/double(knots[i+offset]-knots[i]);
+
+      new_base.col(i)= coeff1*base.col(i) + coeff2*base.col(i+1);
+
+    }
+      
+  //remove the fisrt and last knot before the recursive call 
+  std::vector<double> new_knots;
+  new_knots.insert(new_knots.begin(), knots.begin()+1, knots.end()-1);
+
+
+  find_slope(new_base, new_knots, deriv, u);
+
+}
 
  
