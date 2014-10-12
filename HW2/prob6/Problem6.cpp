0a55c8cbd0be6a6b6221760739527fdeed290f45
Pshriwise <shriwise@wisc.edu>
2014-10-12 15:34:28 -0500

    Added in the correct control points and knot vector.

--- HW2/prob6/Problem6.cpp
+++ HW2/prob6/Problem6.cpp
@@ -4,8 +4,11 @@
 #include <iostream>
 #include <assert.h>
 #include <armadillo>
+#include <math.h>
 #include "formatting.hpp"
 #include "BSpline_algs.hpp"
+
+#define _USE_MATH_DEFINES 
 
 using namespace arma;
 
@@ -16,33 +19,36 @@ int main( int argc, char** argv)
 
 
   //setup knot vector
-  double k [] = { -2, -1, 0, 2, 4, 5, 6, 6};
+  double k [] = { 0, 0, 1, 1, 2, 2, 3, 3,};
   std::vector<double> knots( k, k+ (sizeof(k)/sizeof(k[0]) ) );
 
 
   Mat<double> CPs;
   
-  CPs << 0 << 0 << 0 << endr
-      << 1 << 0 << 0 << endr
-      << 1 << 1 << 0 << endr
-      << 0 << 1 << 0 << endr
-      << 0 << 2 << 0 << endr
-      << 2 << 2 << 0 << endr;
+  CPs << cos(M_PI/6)  << sin(M_PI/6) << 0 << endr //0
+      << 0            << 2           << 0 << endr //1
+      << -cos(M_PI/6) << sin(M_PI/6) << 0 << endr //2
+      << -sqrt(3)     << -1          << 0 << endr //3
+      << 0            << -1          << 0 << endr //4
+      << sqrt(3)      << -1          << 0 << endr //5
+      << cos(M_PI/6)    << sin(M_PI/6) << 0 << endr; //6
 
 
   CPs=CPs.t();
   
 
+  std::cout << CPs << std::endl; 
+
   Mat<double> pnt;
   
-  int degree = 3;
+  int degree = 2;
   double start = knots[degree-1];
-  double end = knots[knots.size()-(degree)];
+  double end = knots[knots.size() - degree];
 
   std::ofstream datafile;
   datafile.open("BSpline.dat");
   
- 
+
   for( double u = start; u <= end; u+=(end-start)/100 )
     {
       blossom_de_boor( degree, CPs, knots, u, pnt);
@@ -57,17 +63,11 @@ int main( int argc, char** argv)
 
   part_header("B");
   
-  double t = 3;
+  double t = 1;
 
   std::cout << "The point of the B-Spline at t= " << t << ":" << std::endl; 
 
   blossom_de_boor(degree, CPs, knots, t, pnt);
-
-  std::cout << pnt << std::endl; 
-
-  std::cout << "The derivative of the B-Spline at t= " << t << ":" << std::endl; 
-
-  blossom_de_boor(degree, CPs, knots, t, pnt, true);
 
   std::cout << pnt << std::endl; 
 
