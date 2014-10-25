5d7e85d5190a110c3a3b5663ee5548d0c83236f3
pshriwise <shriwise@wisc.edu>
2014-10-25 14:33:54 -0500

    Fixed problems with derivative in calc.

--- common/BSpline_algs.cpp
+++ common/BSpline_algs.cpp
@@ -103,10 +103,26 @@ void surf_de_boor( int degree_u, int degree_v,  field<vec> cps, std::vector<doub
     {
       tmp.set_size(1,2);
       //reduce in v
-      for ( unsigned int i = 0 ; i < tbt.n_cols; i ++) tmp(0,1) =  v_interpolant*tmp(0,i) + (1-v_interpolant)*tbt(1,i);
+      for ( unsigned int i = 0 ; i < tbt.n_cols; i ++) tmp(0,i) =  v_interpolant*tbt(0,i) + (1-v_interpolant)*tbt(1,i);
       
       //calculate derivative
       value = degree_u*(tmp(0,1)-tmp(0,0))/(sub_knots_u[1]-sub_knots_u[0]);
+
+      //if this is a nurbs curve, we need to adjust the value of the derivative according to the weights
+      if(nurbs)
+	{
+	  //get the weighted point
+	  surf_de_boor( degree_u, degree_v, cps, knots_u, knots_v, u, v, pw, PNT);
+	  //set our weights
+	  w_prime = value(value.n_rows-1,0);
+	  w = pw(pw.n_rows-1,0); 
+	  p = pw.submat(0,0,pw.n_rows-2,0)/w;
+	  A_prime = value.submat(0,0,value.n_rows-2,0);
+
+	  //reset value to the correct derivative
+	  value = (A_prime - w_prime*p)/w;
+	}
+
     }
     break; 
 
