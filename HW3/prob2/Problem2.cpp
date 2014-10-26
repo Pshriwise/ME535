a81217dd5b5b8a767e8beb93fa5f68e25d6a0936
Pshriwise <shriwise@wisc.edu>
2014-10-26 16:28:02 -0500

    Removed warning from uv derivative calc. Pretty confident in that now. Added some output to make Problem 2 cleaner.

--- HW3/prob2/Problem2.cpp
+++ HW3/prob2/Problem2.cpp
@@ -14,15 +14,25 @@ using namespace arma;
 int main( int argc, char** argv)
 {
 
+  part_header("A");
+
   //setup knot vector
+  std::cout << "The knot values in u and v are: " << std::endl;  
   double ku [] = { 0, 0, 1, 1 };
   std::vector<double> knots_u( ku, ku+ (sizeof(ku)/sizeof(ku[0]) ) );
+  std::cout << std::endl << "U" << std::endl;
+  for(unsigned int i = 0; i < knots_u.size(); i++ )std::cout << knots_u[i] << std::endl; 
+
 
   double kv [] = { 0, 1 };
   std::vector<double> knots_v( kv, kv+ (sizeof(kv)/sizeof(kv[0]) ) );
+  std::cout << std::endl << "V" << std::endl;  
+  for(unsigned int i = 0; i < knots_v.size(); i++ )std::cout << knots_v[i] << std::endl; 
 
+  std::cout << "The weight values are:" << std::endl; 
   double w [] = { 1, cos(M_PI/4), 1 };
   std::vector<double> weights( w, w + (sizeof(w)/sizeof(w[0]) ) );
+  for(unsigned int i = 0; i < weights.size(); i++ )std::cout << weights[i] << std::endl; 
 
   field<vec> CPS(2,3);
   // enter CPs in respective groups for v
@@ -34,7 +44,8 @@ int main( int argc, char** argv)
   CPS(1,1) << 1 << 1 << 1 << 1;
   CPS(1,2) << 0 << 1 << 1 << 1;
 
-  //std::cout << CPS << std::endl; 
+  std::cout << "The matrix of control points looks like" << std::endl; 
+  std::cout << CPS << std::endl; 
 
   //apply weights
 
@@ -64,13 +75,19 @@ int main( int argc, char** argv)
 	  Surf.insert_cols(Surf.n_cols,pnt.col(0));
 	}
       }
+
+  part_header("B");
   pnt.clear();
+  std::cout << "The value of the surface at u=0.5 and v=0.5 is:" << std::endl;
   surf_de_boor( u_deg, v_deg, CPS, knots_u, knots_v, 0.5, 0.5, pnt, PNT, true);
   std::cout << pnt << std::endl; 
+  std::cout << "The value of the derivative in u at u=0.5 and v=0.5 is:" << std::endl;
   surf_de_boor( u_deg, v_deg, CPS, knots_u, knots_v, 0.5, 0.5, pnt, DERIV_U, true);
   std::cout << pnt << std::endl; 
+  std::cout << "The value of the deriative in v at u=0.5 and v=0.5 is:" << std::endl;
   surf_de_boor( u_deg, v_deg, CPS, knots_u, knots_v, 0.5, 0.5, pnt, DERIV_V, true);
   std::cout << pnt << std::endl; 
+  std::cout << "The value of the derivative in u and v at u=0.5 and v=0.5 is:" << std::endl;
   surf_de_boor( u_deg, v_deg, CPS, knots_u, knots_v, 0.5, 0.5, pnt, DERIV_UV, true);
   std::cout << pnt << std::endl; 
 
