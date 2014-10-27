d3797c8430eedd788e8d7113d086b90b72ee1def
Pshriwise <shriwise@wisc.edu>
2014-10-26 22:52:20 -0500

    Added a function for calculating Bezier patches.

--- common/Bezier_algs.cpp
+++ common/Bezier_algs.cpp
@@ -97,3 +97,41 @@ void de_cast_prime( double u, double order, Mat<double> CP, Mat<double> &value, 
     }
 
 } // end of de_cast
+
+
+
+void Bezier_bicubic_patch( field<vec> cps, double u, double v, Mat<double> &pnt, bool nurbs) 
+{
+
+
+  Row<double> U,V;
+  U << pow(u,3) << pow(u,2) << u << 1;
+  V << pow(v,3) << pow(v,2) << v << 1; 
+
+  Mat<double> M;
+  
+  M << -1  <<  3  << -3  << 1 << endr
+    <<  3  << -6  <<  3  << 0 << endr
+    << -3  <<  3  <<  0  << 0 << endr
+    <<  1  <<  0  <<  0  << 0 << endr; 
+  
+  Mat<double> X(4,4),Y(4,4),Z(4,4),W(4,4);
+  for(unsigned int i = 0; i < cps.n_rows; i++)
+    for(unsigned int j = 0; j < cps.n_cols; j++)
+      {{
+	  X(i,j) = cps(i,j)(0);
+	  Y(i,j) = cps(i,j)(1);
+	  Z(i,j) = cps(i,j)(2);
+	  if(nurbs) W(i,j) = cps(i,j)(3);
+	  
+	}}
+  pnt.resize(cps(0,0).n_rows,1); 
+  Mat<double> temp; 
+  temp = U*M*X*M.t()*V.t(); pnt(0,0) = temp(0,0); 
+  temp = U*M*Y*M.t()*V.t(); pnt(1,0) = temp(0,0); 
+  temp = U*M*Z*M.t()*V.t(); pnt(2,0) = temp(0,0); 
+  if(nurbs) temp = U*M*W*M.t()*V.t(); pnt(3,0) = temp(0,0); 
+
+
+
+}
--- common/Bezier_algs.hpp
+++ common/Bezier_algs.hpp
@@ -9,3 +9,6 @@ using namespace arma;
 void de_cast( double u, Mat<double> CP, Mat<double> &pnt, std::vector<Mat<double > > &plot_data );
 
 void de_cast_prime( double u, double order, Mat<double> CP, Mat<double> &value, std::vector<Mat<double > > &plot_data );
+
+
+void Bezier_bicubic_patch( field<vec> cps, double u, double v, Mat<double> &pnt, bool nurbs = false );
