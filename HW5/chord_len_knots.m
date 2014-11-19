d891c434342eaabcfc6b18758b4840976d7b2ee7
Pshriwise <shriwise@wisc.edu>
2014-11-19 10:57:12 -0600

    Added a matlab function for calculating chord length based knot values given a set of data points.

---
+++ HW5/chord_len_knots.m
@@ -0,0 +1,20 @@
+function knot_values = chord_len_knots(data_pnts)
+
+
+[num_pnts dim] = size(data_pnts); 
+
+chords = zeros(1,num_pnts-1);
+
+%calculate chord lengths
+for i = 1:num_pnts-1
+    chords(i) = sqrt(sum((data_pnts(i+1,:)-data_pnts(i,:)).^2));
+end
+
+%get the un-normalized knot values
+knots = zeros(1,num_pnts)
+for i = 2:num_pnts
+    knots(i)= sum(chords(1:i-1));
+end
+
+%normalize and return
+knot_values = knots./sum(chords);
