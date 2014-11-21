bc8221fb5039949174d1a1a3b88c129af96f4c11
Pshriwise <shriwise@wisc.edu>
2014-11-20 22:09:18 -0600

    Added a new path and derivative to the tentacle program!

--- proj/test_cir.m
+++ proj/test_cir.m
@@ -2,11 +2,17 @@
 
 figure(1);
 hold on;
+%arm path
+P = [ 1 2 5; 3 6 8; 5 3 9; 10 8 10];
+t = [ 0 0 0 1 1 1];
+k = 3;
+n = 20;
 
-for u = 0.01:0.1:1
+for u = 0.01:0.01:1
 
-    center = [ u u^2 u+u^2 ];
-    n = [ u u^2 1+2*u];
+    center = deBoor(k,t,P,u,3);
+    du = 0.0001;
+    n = (1/du)*(deBoor(k,t,P,u+du,3)-deBoor(k,t,P,u,3));
     n = n./norm(n);
     pnts = circle(get_circle_rad(u), center, n);
     scatter3(pnts(:,1),pnts(:,2),pnts(:,3))
