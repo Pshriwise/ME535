365b5745d5bcf59db24ddc292bf9dda964b10473
Pshriwise <shriwise@wisc.edu>
2014-11-28 16:35:21 -0600

    Generalized the control point generation for circles to take any number of sections.

--- proj/gen_circle_cps.m
+++ proj/gen_circle_cps.m
@@ -1,7 +1,19 @@
 
-function [CPs] = gen_circle_cps(rad,center, n)
-
+function [CPs] = gen_circle_cps(rad,center, n, sections)
+% Function which supplies the control points and weights for a circle
+% (weights are supplied in an added dimension)
+% rad - radius of the circle
+% center - centerpoint of the circle
+% n - normal vector to the center of the circle 
+% sections - number of sections the circle is broken up into
 %perturb normal if needed
+
+%default setion value 
+if nargin < 4
+    sections = 4;
+end 
+
+% corner cases for then normal vector calculation
 if [ 0 1 0 ] == n 
     n = [ 1e-7 1 0];
 end
@@ -20,26 +32,31 @@ dum = [ -n(3) 0 n(1) ];
 perp = cross(dum, n);
 perp = perp./norm(perp);
 
-%asssumes the normal vector is of unit length
-x = center(1);
-y = center(2);
-z = center(3);
- 
-CPs = [x y z; x y z; x y z; x y z; x y z; x y z; x y z; x y z; x y z];
+%set center point values (make sure shape of center is correct)
+c(1) = center(1);
+c(2) = center(2);
+c(3) = center(3);
 
-for i = 1:9
+%determine our roataion angle 
+angle = 360/(2*sections);
 
+for i = 1:(2*sections+1)
+
+    %start at the center 
+    CPs(i,:) = c;
     %set magnitude for this point
     if ( 0 == mod(i,2) ) mag = sqrt(2)*rad; 
     else mag = rad; 
     end
-    %rotate vector by 45 degrees
-    if ( 1 ~= i )perp = cosd(45)*perp +sind(45)*cross(perp,n); end
+    %rotate vector by our angle
+    if ( 1 ~= i )perp = cosd(angle)*perp +sind(angle)*cross(perp,n); end
     
+    %set the detla vector relative to the center
     v = mag*perp;
 
     CPs(i,:) = CPs(i,:)+v;
-    
+
+    %renormalize the perp vector
     perp = perp./norm(perp);
                                 
 end
