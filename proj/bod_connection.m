33b9cb7ee938be2554b37b279927c6638286dc60
Pshriwise <shriwise@wisc.edu>
2014-11-29 13:46:10 -0600

    Altered the body connection points so that they meet at the origin for each arm.

--- proj/bod_connection.m
+++ proj/bod_connection.m
@@ -28,10 +28,20 @@ for i = 3:13
     adj_surf_CPs(i,1:5,1:3) = adj_surf_CPs(i-1,1:5,1:3).*0.8;
     adj_surf_CPs(i,9,1:3) = adj_surf_CPs(i-1,9,1:3).*0.8;
     adj_surf_CPs(i,6:8,1) = adj_surf_CPs(i-1,6:8,1).*0.95;
-    adj_surf_CPs(i,6:8,2) = adj_surf_CPs(i-1,6:8,2).*1.02;
-    adj_surf_CPs(i,6:9,3) = adj_surf_CPs(i-1,6:9,3).*1.04;
+    adj_surf_CPs(i,6:8,2) = adj_surf_CPs(i-1,6:8,2).*1.01;
+    adj_surf_CPs(i,6:9,3) = adj_surf_CPs(i-1,6:9,3).*1.02;
     adj_surf_CPs(i,:,4) = adj_surf_CPs(i-1,:,4); %bring weights along!
 end
+
+i = i+1; 
+%make sure the bottom points go to the origin 
+    adj_surf_CPs(i,1:4,1:3) = adj_surf_CPs(i-1,1:4,1:3).*0; % <--
+    adj_surf_CP2(i,5,1:3) = adj_surf_CPs(i-1,5,1:3).*0;
+    adj_surf_CPs(i,9,1:3) = adj_surf_CPs(i-1,9,1:3).*0;
+    adj_surf_CPs(i,6:8,1) = adj_surf_CPs(i-1,6:8,1).*0.95;
+    adj_surf_CPs(i,6:8,2) = adj_surf_CPs(i-1,6:8,2).*1.01;
+    adj_surf_CPs(i,6:9,3) = adj_surf_CPs(i-1,6:9,3).*1.02;
+    adj_surf_CPs(i,:,4) = adj_surf_CPs(i-1,:,4); %bring weights along!
 
 
 %create rotation matrix 
@@ -39,10 +49,10 @@ R = [ cosd(angle) -sind(angle) 0; sind(angle) cosd(angle) 0; 0 0 1];
 
 
 
-
+[a b c] = size(adj_surf_CPs); 
 %plot3(adj_surf_CPs(:,:,1),adj_surf_CPs(:,:,2),adj_surf_CPs(:,:,3),'o')
 % %apply weights 
-for i = 1:13
+for i = 1:a
     for j = 1:b
         adj_surf_CPs(i,j,1:c-1) = R*squeeze(adj_surf_CPs(i,j,1:c-1));
         adj_surf_CPsW(i,j,1:c-1) = squeeze(adj_surf_CPs(i,j,1:c-1)).*adj_surf_CPs(i,j,c);
